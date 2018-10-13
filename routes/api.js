df064ab791b68ef6cf85e2f140eb2d6031049123
James Xu <xujames007@gmail.com>
2018-10-13 17:23:10 -0400

    Auth mostly done

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,43 @@
 let express = require('express');
+let mysql = require('mysql');
+let jwt = require('jsonwebtoken');
+let util = require('util');
+
 let router = express.Router();
+
+let pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'API'});
+});
+
+router.post('/viewAllApplications', function (req, res, next) {
+    let responseJSON = {
+        status: 200,
+        content: null
+    };
+    jwt.verify(req.body.token, process.env.JWT_SECRET, function (err, decoded) {
+        if (role < 2) {
+            pool.query("SELECT * FROM applications", function (error, results, fields) {
+                if (!error) {
+                    responseJSON.content = results;
+                } else {
+                    responseJSON.status = 500;
+                }
+                res.send(responseJSON);
+            })
+        } else {
+            responseJSON.status = 403;
+            res.send(responseJSON);
+        }
+    })
 });
 
 module.exports = router;
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,10 @@ let argon2 = require('argon2');
 let mysql = require('mysql');
 let jwt = require('jsonwebtoken');
 let util = require('util');
+let uuidv1 = require('uuid/v1');
+let sgMail = require('@sendgrid/mail');
+let hb = require('handlebars');
+let fs = require('fs');
 
 let router = express.Router();
 
@@ -13,11 +17,12 @@ let pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
+sgMail.setApiKey(process.env.SG_API);
 
 router.post('/login', function (req, res, next) {
-    let user = req.body.username;
+    let username = req.body.username;
     let password = req.body.password;
-    let queryString = util.format("SELECT * FROM users WHERE username='%s';", user);
+    let queryString = util.format("SELECT * FROM users WHERE username='%s';", username);
     let responseJSON = {
         status: 200,
         token: null,
@@ -31,16 +36,21 @@ router.post('/login', function (req, res, next) {
         } else {
             argon2.verify(results[0].password, password).then(match => {
                 if (match) {
-                    let tokenJSON = {
-                        username: user,
-                        id: parseInt(results[0].id),
-                        role: results[0].role,
-                        application: results[0].application,
-                    };
-                    let token = jwt.sign(tokenJSON, 'secret', {
-                        expiresIn: 60 * 60
-                    });
-                    responseJSON.token = token;
+                    if (results[0].verifytoken === '') {
+                        let tokenJSON = {
+                            username: username,
+                            id: parseInt(results[0].id),
+                            role: results[0].role,
+                            application: results[0].application,
+                        };
+                        let token = jwt.sign(tokenJSON, process.env.JWT_SECRET, {
+                            expiresIn: 60 * 60
+                        });
+                        responseJSON.token = token;
+                    } else {
+                        responseJSON.status = 403;
+                        responseJSON.message = "Verify Email First";
+                    }
 
                 } else {
                     responseJSON.status = 403;
@@ -53,17 +63,84 @@ router.post('/login', function (req, res, next) {
 });
 
 router.post('/signup', function (req, res, next) {
-    let user = req.body.username;
+    let username = req.body.username;
+    let name = req.body.name;
+    let email = req.body.email;
     let password = req.body.password;
+    let verifyToken = uuidv1();
+    let role = 2;
+    let responseJSON = {
+        status: 200,
+        message: 'OK'
+    };
     console.log(user, password);
     argon2.hash(password, {
         type: argon2.argon2i
     }).then(hash => {
-        res.send(hash)
+        const queryString = util.format("INSERT INTO users (username, name, password, email, role, verifytoken) VALUES ('%s', '%s', '%s', '%s', '%s')", username, name, hash, email, 2, verifyToken);
+        pool.query(queryString, function (error, results, fields) {
+            if (error) {
+                responseJSON.status = 500;
+                responseJSON.message = "Internal Error";
+                res.send(responseJSON);
+            } else {
+                fs.readFile('../emailTemplates/emailVerifyTemplate.hbs', 'UTF-8', function (err, contents) {
+                    if (err) {
+                        responseJSON.status = 500;
+                        responseJSON.message = "File I/O Error";
+                        res.send(responseJSON)
+                    } else {
+                        let htmlContent = hb.compile(contents);
+                        let data = {
+                            verifyURL: util.format("https://register@%s/auth/verify/%s", process.env.DOMAIN, verifyToken)
+                        };
+                        const result = htmlContent(data);
+                        const msg = {
+                            to: email,
+                            from: util.format("register@%s", process.env.DOMAIN),
+                            subject: "Please Verify Your Email",
+                            text: "Your email client does not support HTML",
+                            html: result
+                        };
+                        sgMail.send(msg);
+                        res.send(responseJSON);
+                    }
+                });
+            }
+        })
     }).catch(err => {
         res.sendStatus(500);
         console.log(err);
     });
+});
+
+router.get('/verify/:verifyToken', function (req, res, next) {
+    let responseJSON = {
+        status: 200,
+        message: 'OK'
+    };
+    const verifyToken = req.params.verifyToken;
+    const queryString = util.format("SELECT id FROM users WHERE verifytoken='%s'", verifyToken);
+    pool.query(queryString, function (error, results, fields) {
+        if (error) {
+            responseJSON.status = 500;
+            responseJSON.message = "Invalid Token";
+            res.send(responseJSON);
+        } else {
+            if (results.length > 0) {
+                const updateQueryString = util.format("UPDATE users set verifytoken='' WHERE verifytoken = '%s'", verifyToken);
+                pool.query(updateQueryString, function (error, results, fields) {
+                    if (error) {
+                        responseJSON.status = 500;
+                        responseJSON.message = "MySQL Error";
+                        res.send(responseJSON);
+                    } else {
+                        res.send(responseJSON);
+                    }
+                })
+            }
+        }
+    })
 });
 
 module.exports = router;
