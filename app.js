30f6dbb5bbd2b7ba2d1a47f9d6a8845055da9c24
David Hui <davidhui@davesoftllc.com>
2018-10-14 20:57:58 -0400

    Do not index verifytoken and lastChanged. Changed all imports to const

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 require('dotenv').config();
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
-let apiRouter = require('./routes/api');
-let authRouter = require('./routes/auth');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const apiRouter = require('./routes/api');
+const authRouter = require('./routes/auth');
 
 let app = express();
 
